05ebe2e8e9efdf93f662f4f42cf5b93fc68ee4eb
Natalya Lebedeva <natashaswan.ca@gmail.com>
2023-01-06 11:37:58 -0800

    integrated opnai api

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,55 @@
 import React, { useState, useRef } from "react";
 
-import Button from './Button';
+import { Configuration, OpenAIApi } from "openai";
 
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+    });
+
+const openai = new OpenAIApi(configuration);
+console.log(process.env)
 const Input: React.FC = () => {
+    const [imageURL, setImageURL] = useState<string | undefined >(undefined);
+
+    //
+    //s
+    //
+
     const inputValueRef = useRef<HTMLInputElement>(null);
+
+    //
+    //
+    //
+
+    const onClickHandler = () => {
+        const enteredText: string | undefined = inputValueRef?.current?.value;
+        //
+        //
+        const generateImage = async () =>{
+            if(enteredText){
+                console.log(enteredText);
+                const imageParameters: {prompt: string} = {
+                    prompt: enteredText,
+                }
+
+                const response = await openai.createImage(imageParameters);
+                console.log(response);
+                const urlData: string | undefined = response.data.data[0].url;
+                console.log(response.data.data);
+                setImageURL(urlData);
+            }
+
+        }
+        generateImage();
+    }
 
     return(
             <div className="input">
-                <label htmlFor="input-text"></label>
+                <label htmlFor="input-text">Image Description</label>
                 <input type='text' id='input-text' placeholder="image description" ref={inputValueRef}></input>
-                <Button inputValue={inputValueRef}/>
+                <button onClick={onClickHandler}>
+                    Generate image
+                </button>
             </div> 
         )
 }
