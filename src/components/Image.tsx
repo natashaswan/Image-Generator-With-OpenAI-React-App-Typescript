ea44ecb24bc7e9978314f5f9c8808035e8bf1e05
natashaswan <91715868+natashaswan@users.noreply.github.com>
2023-01-16 09:35:42 -0800

    add small format changes

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Rings } from 'react-loader-spinner';
-import {SVGImg} from '../assets/PackageSVG';
+import { SVGImg } from '../assets/PackageSVG';
 
 const Image = ({url, fetching, error}: {url:string | undefined, fetching: boolean | undefined, error: boolean | undefined}) => {
     return(
         <>
             <h2>
-                Provide a prompt and see your image below
+                Provide a prompt and see your image below!
             </h2>
             <div className="image">
                 {fetching ? <Rings/> : error ? <div>Error occured. Try again.</div> : url ? <img className="image" src={url} alt='ai generated'/> : <SVGImg/>}
